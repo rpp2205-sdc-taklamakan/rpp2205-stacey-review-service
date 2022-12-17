57f666d2609810df893e04f573a0fe9a3ad7f6f0
Stacey Pereira <staceypereira1@gmail.com>
2022-12-17 13:51:35 -0800

    added socketPath

--- database/db.js
+++ database/db.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  socketpath: process.env.DB_SOCKETPATH
 });
 
 const db = Promise.promisifyAll(connection, { multiArgs: true});
