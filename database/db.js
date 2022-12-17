b38943bb04834600b7180364d2d1f95574238a67
Stacey Pereira <staceypereira1@gmail.com>
2022-12-17 14:56:50 -0800

    changed to mysql2

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Promise = require("bluebird");
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
