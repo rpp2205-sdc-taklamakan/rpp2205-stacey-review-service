4f29845e0272f392cef3ee68a5e5ebec152823a0
Stacey Pereira <staceypereira1@gmail.com>
2022-12-19 19:04:04 -0800

    added route for loader io to send .txt file

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.send("Reviews Service");
 });
 
+app.get('/loaderio-1fc425625de99b0b2d3a4847dd646271', (req, res) => {
+  if(err) {
+    res.status(500).send(err);
+  }
+  res.status(200).send('../loaderio-1fc425625de99b0b2d3a4847dd646271.txt');
+});
 
 app.get('/reviews', Controller.getReviews); //DONE
 
